57d78c00c8026ab16cd6f8c7872c19ba03ca9bec
nachiket273 <nachiket.tanksale@gmail.com>
2019-08-23 01:04:30 +0530

    Added cosine increase-decrease support for one cycle.

--- OneCycle.py
+++ OneCycle.py
@@ -1,3 +1,5 @@
+import math
+
 class OneCycle(object):
     """
     In paper (https://arxiv.org/pdf/1803.09820.pdf), author suggests to do one cycle during 
@@ -31,33 +33,52 @@ class OneCycle(object):
                         This value is also used to decide how much we annihilate the learning 
                         rate below lower learning rate.
                         The default value is 10.
+
+        use_cosine      Use cosine annealation instead of linear to change learning rate and 
+                        momentum. 
+                        The default value is False
     """
-    def __init__(self, nb, max_lr, momentum_vals=(0.95, 0.85), prcnt= 10 , div=10):
+    def __init__(self, nb, max_lr, momentum_vals=(0.95, 0.85), prcnt= 10, div=10, use_cosine=False):
         self.nb = nb
         self.div = div
-        self.step_len =  int(self.nb * (1- prcnt/100)/2)
         self.high_lr = max_lr
         self.low_mom = momentum_vals[1]
         self.high_mom = momentum_vals[0]
-        self.prcnt = prcnt
+        self.use_cosine = use_cosine
+        if self.use_cosine:
+            self.prcnt = 0
+        else:
+            self.prcnt = prcnt
         self.iteration = 0
         self.lrs = []
         self.moms = []
+        if self.use_cosine:
+            self.step_len =  int(self.nb / 4)
+        else:
+            self.step_len =  int(self.nb * (1- prcnt/100)/2)
         
     def calc(self):
+        if self.use_cosine:
+            lr = self.calc_lr_cosine()
+            mom = self.calc_mom_cosine()
+        else:
+            lr = self.calc_lr()
+            mom = self.calc_mom()
         self.iteration += 1
-        lr = self.calc_lr()
-        mom = self.calc_mom()
         return (lr, mom)
         
     def calc_lr(self):
-        if self.iteration==self.nb:
+        if self.iteration ==  0:
+            self.lrs.append(self.high_lr/self.div)
+            return self.high_lr/self.div
+        elif self.iteration == self.nb:
             self.iteration = 0
             self.lrs.append(self.high_lr/self.div)
             return self.high_lr/self.div
-        if self.iteration > 2 * self.step_len:
+        elif self.iteration > 2 * self.step_len:
             ratio = (self.iteration - 2 * self.step_len) / (self.nb - 2 * self.step_len)
-            lr = self.high_lr * ( 1 - 0.99 * ratio)/self.div
+            #lr = self.high_lr * ( 1 - 0.99 * ratio)/self.div
+            lr = (self.high_lr / self.div) * (1- ratio * (1 - 1/self.div))
         elif self.iteration > self.step_len:
             ratio = 1- (self.iteration -self.step_len)/self.step_len
             lr = self.high_lr * (1 + ratio * (self.div - 1)) / self.div
@@ -66,13 +87,16 @@ class OneCycle(object):
             lr = self.high_lr * (1 + ratio * (self.div - 1)) / self.div
         self.lrs.append(lr)
         return lr
-    
+
     def calc_mom(self):
-        if self.iteration==self.nb:
+        if self.iteration == 0:
+            self.moms.append(self.high_mom)
+            return self.high_mom
+        elif self.iteration == self.nb:
             self.iteration = 0
             self.moms.append(self.high_mom)
             return self.high_mom
-        if self.iteration > 2 * self.step_len:
+        elif self.iteration > 2 * self.step_len:
             mom = self.high_mom
         elif self.iteration > self.step_len:
             ratio = (self.iteration -self.step_len)/self.step_len
@@ -80,5 +104,39 @@ class OneCycle(object):
         else :
             ratio = self.iteration/self.step_len
             mom = self.high_mom - ratio * (self.high_mom - self.low_mom)
+        self.moms.append(mom)
+        return mom
+
+    def calc_lr_cosine(self):
+        if self.iteration ==  0:
+            self.lrs.append(self.high_lr/self.div)
+            return self.high_lr/self.div
+        elif self.iteration == self.nb:
+            self.iteration = 0
+            self.lrs.append(self.high_lr/self.div)
+            return self.high_lr/self.div
+        elif self.iteration > self.step_len:
+            ratio = (self.iteration -self.step_len)/(self.nb - self.step_len)
+            lr = (self.high_lr/self.div) + 0.5 * (self.high_lr - self.high_lr/self.div) * (1 + math.cos(math.pi * ratio))
+        else :
+            ratio = self.iteration/self.step_len
+            lr = self.high_lr - 0.5 * (self.high_lr - self.high_lr/self.div) * (1 + math.cos(math.pi * ratio))
+        self.lrs.append(lr)
+        return lr
+
+    def calc_mom_cosine(self):
+        if self.iteration == 0:
+            self.moms.append(self.high_mom)
+            return self.high_mom
+        elif self.iteration == self.nb:
+            self.iteration = 0
+            self.moms.append(self.high_mom)
+            return self.high_mom
+        elif self.iteration > self.step_len:
+            ratio = (self.iteration -self.step_len)/(self.nb - self.step_len)
+            mom = self.high_mom - 0.5 * (self.high_mom - self.low_mom) * (1 + math.cos(math.pi * ratio))
+        else :
+            ratio = self.iteration/self.step_len
+            mom = self.low_mom + 0.5 * (self.high_mom - self.low_mom) * (1 + math.cos(math.pi * ratio))
         self.moms.append(mom)
         return mom
